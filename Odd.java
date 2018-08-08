000d4c13caf1b89f7e7dfacab11d4ddaf23e8cfd
SuryaPrasanth1437 <37958227+SuryaPrasanth1437@users.noreply.github.com>
2018-08-08 13:46:46 +0530

    Add files via upload

---
+++ Odd.java
@@ -0,0 +1,21 @@
+package program;
+
+public class Odd
+{
+
+	public static void main(String[] args)
+	{
+		int i;
+		for(i=1;i<=101;i++)
+		{
+			if(i%2!=0)
+			{
+				if(i%5!=0)
+					System.out.println(i);
+			}
+		}
+		
+
+	}
+
+}
